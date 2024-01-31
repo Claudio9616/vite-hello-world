bc7495feb930dbe70423443843455ec7fb7fe513
Claudio9616 <clares9611@gmail.com>
2024-01-31 18:08:07 +0100

    add app completed

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data: () => ({
+        name: 'DELLA PRIMA APPLICAZIONE'
+    })
+}
+</script>
+<template>
+    <h1>TITOLO {{ name }}</h1>
+    <img src="../../01.jpeg" alt="">
+</template>
+<style scoped>
+h1 {
+    color: brown;
+}
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data: () => ({
+        name: 'DELLA PRIMA APPLICAZIONE',
+        image: '02.jpeg'
+    }),
+    methods: {
+        getImagePath(image) {
+            const url = new URL(`../assets/${image}`, import.meta.url)
+            return url.href
+        }
+    }
+}
+</script>
+<template>
+    <h2>SOTTOTITOLO {{ name }}</h2>
+    <img :src="getImagePath(image)" alt="">
+</template>
+<style scoped>
+h2 {
+    color: cadetblue;
+}
+</style>
